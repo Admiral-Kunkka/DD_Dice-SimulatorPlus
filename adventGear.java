99bd5e7cfc6be5adad736a4ae2a437b380e5a4dd
BurningSushi <babydoubles@gmail.com>
2015-05-27 09:15:20 -0700

    Adding objects, AdventuringGear, Armor, Weapon
    Started and finished these three classes to work as objects to be
    created and kept track of in the program.

---
+++ adventGear.java
@@ -0,0 +1,18 @@
+public class adventGear{
+   
+   String name;
+   int weight;
+   int price;
+   
+   public adventGear(){}
+   
+   //Set Methods
+   public void setName(String x){name = x;}
+   public void setWeight(int x){weight = x;}
+   public void setPrice(int x){price = x;}
+   
+   //Get methods
+   public String getName(){return name;}
+   public int getWeight(){return weight;}
+   public int getPrice(){return price;}
+}

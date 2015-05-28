fc5fcce5cedd7083b1e0d4c481ecdbe9dc73fb3a
BurningSushi <babydoubles@gmail.com>
2015-05-27 17:46:16 -0700

    Revert "Revert "Created At-Will and Special Ability class files""
    This reverts commit db8729e99964df63b70f4915e6fe5e37de6234c0.

---
+++ SpclAbility.java
@@ -0,0 +1,17 @@
+public class SpclAbility{ //Special Abilities
+   //Includes Encounter and Daily ability types
+   
+   String name;
+   String info;
+   
+   public SpclAbility(){}
+   
+   //Set Methods
+   public void setName(String x){name = x;}
+   public void setInfo(String x){info = x;}
+   
+   //Get Methods
+   public String getName(){return name;}
+   public String getInfo(){return info;}
+   
+}

cb19ee4c4fb1f930c3637f542f9f4e2a6fdac381
Admiral-Kunkka <jacob.contreras96@gmail.com>
2015-06-03 15:32:30 -0700

    Got to MiscFORT

--- UIEditor.java
+++ UIEditor.java
@@ -130,5 +130,89 @@ public class UIEditor{
       System.out.print("Describe what your using or anything extra you want >> ");
          v.setInfo(in.nextLine().trim());
    }
+   public static void editCharacter(){
+      Character k = new Character();
+      System.out.println("Name of your character >> ");
+         k.setName(in.nextLine().trim());
+      System.out.println("Your name >> ");
+         k.setOwner(in.nextLine().trim());
+      System.out.println("Name of your class type >> ");
+         k.setClassType(in.nextLine().trim());
+      System.out.println("What is your Paragon Path >> ");
+         k.setParagonPath(in.nextLine().trim());
+      System.out.println("What is your Epic Destiny >> ");
+         k.setEpicDestiny(in.nextLine().trim());
+      System.out.println("What is your total XP >> ");
+         k.setTotalXP(in.nextLine().trim());
+      System.out.println("What is the Race of your character >> ");
+         k.setRace(in.nextLine().trim());
+      try{
+         System.out.print("What is the Age of your character >> ");
+         k.setAge(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      } 
+      System.out.println("What is the gender of your character(M/F) >> ");
+         k.setGender(in.nextLine().trim());
+      System.out.println("What is the height of your charcter >> ");
+         k.setHeight(in.nextLine().trim());
+      try{
+         System.out.print("What is the weight of your character >> ");
+         k.setWeight(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      System.out.println("What is the Alignment of your charcter >> ");
+         k.setAlignment(in.nextLine().trim());
+      System.out.println("What is the Deity of your charcter >> ");
+         k.setDeity(in.nextLine().trim());
+      try{
+         System.out.print("What is the level of your character >> ");
+         k.setLevel(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Ability Mod of your character >> ");
+         k.setAbilityMod(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Race Speed of your character >> ");
+         k.setRaceSpeed(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Race defense of your character >> ");
+         k.setRaceDef(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Race Attack of your character >> ");
+         k.setRaceAtk(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Base Attack of your character >> ");
+         k.setMiscBA(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+      try{
+         System.out.print("What is the Armor Class of your character >> ");
+         k.setMiscAC(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }try{
+         System.out.print("What is the Fortification of your character >> ");
+         k.setMiscFORT(Integer.parseInt(in.nextLine().trim()));
+      }catch(Exception e){
+         System.out.println("That's not a number you idiot!");
+      }
+   }
    
 }
